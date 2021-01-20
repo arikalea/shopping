69f7261a42d0a09f137465def8ffb18458ff517d
Arika Turing <ablenker@gmail.com>
2021-01-20 09:24:34 -0700

    Create add multiple items test

--- test/vendor_test.rb
+++ test/vendor_test.rb
@@ -25,5 +25,15 @@ class VendorTest < Minitest:: Test
     @vendor.stock(@item1, 30)
 
     assert_equal ({@item1 => 30}), @vendor.inventory
+    assert_equal 30, @vendor.check_stock(@item1)
+  end
+
+  def test_stock_multiple_items
+    @vendor.stock(@item1, 30)
+    @vendor.stock(@item1, 25)
+    @vendor.stock(@item2, 12)
+
+    assert_equal 55, @vendor.check_stock(@item1)
+    assert_equal ({@item1 => 55, @item2 => 12})
   end
 end
